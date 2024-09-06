d3b3bc655ed71cb1c4203f6ecc81d68c69df3731
Ritu Raj <rituraj995kumar@gmail.com>
2024-09-06 15:00:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ def home():
         "message": "hello let be friend"
     }
     return jsonify(response)
+@app.route('/about')
+def about():
+    response = {
+        "status": 200,
+        "message": "this is about component"
+    }
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run()
