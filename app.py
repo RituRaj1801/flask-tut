ea52e1f33115162d8f63a1122f6000bc993f3bdc
Ritu Raj <rituraj995kumar@gmail.com>
2024-09-15 10:25:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify, render_template
+import youtube_dl
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    response = {
-        "status": 200,
-        "message": "hello let be friend"
-    }
-    return jsonify(response)
-@app.route('/about')
-def about():
-    response = {
-        "status": 200,
-        "message": "this is about component"
-    }
-    return jsonify(response)
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download_video():
+    try:
+        data = request.get_json()
+        video_url = data['url']
+
+        # Logic for downloading the video
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': './%(title)s.%(ext)s'
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([video_url])
+
+        return jsonify({"message": "Download started!"}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
